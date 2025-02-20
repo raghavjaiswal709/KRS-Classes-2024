e45e07fb0bf46ecdc02880b1a5a3564a89a90e02
Raghav jaiswal <125604612+raghavjaiswal709@users.noreply.github.com>
2025-02-20 22:07:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,21 +10,30 @@ dotenv.config();
 connectDB();
 
 app.use(express.json());
-app.use(cors({
-    origin: [
-        "https://krs-classes-2024-client.vercel.app",
-        "https://krs-classes-2024-client.vercel.app/signup"
-        "https://krs-classes-2024-client-59jtk0cr0-raghav-jaiswals-projects.vercel.app"
-    ],
-    methods: ["POST", "GET", "PATCH", "DELETE"],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: [
+//         "https://krs-classes-2024-client.vercel.app",
+//         "https://krs-classes-2024-client.vercel.app/signup"
+//         "https://krs-classes-2024-client-59jtk0cr0-raghav-jaiswals-projects.vercel.app"
+//     ],
+//     methods: ["POST", "GET", "PATCH", "DELETE"],
+//     credentials: true
+// }));
 
 // app.use(cors({
 //     origin: "*", // Allows requests from any origin
 //     methods: ["POST", "GET", "PATCH", "DELETE"],
 //     credentials: true
 // }));
+
+app.use(cors({
+    origin: (origin, callback) => {
+        callback(null, true); // Allows requests from any origin
+    },
+    methods: ["POST", "GET", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true // Allows cookies & authentication headers
+}));
 
 
 
